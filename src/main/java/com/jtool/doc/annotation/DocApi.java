b5fca306d0fe47684e97ad63e9d83267055515bf
jialechan <jiale.chan@gmail.com>
2015-01-23 11:54:49 +0800

    init

---
+++ src/main/java/com/jtool/doc/annotation/DocApi.java
@@ -0,0 +1,15 @@
+package com.jtool.doc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DocApi {
+	String name();
+	double chapter();
+	String description() default "";
+	String host() default "host(get from initialization)";
+}
---
+++ src/main/java/com/jtool/doc/annotation/DocParam.java
@@ -0,0 +1,14 @@
+package com.jtool.doc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DocParam {
+	String value();
+	boolean required() default true;
+	String comment() default "";
+}
